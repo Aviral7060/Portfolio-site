64d68b2d82fc1e2ccdb331e1c714ebf1205d802b
Aviral-housing <aviral.jain@housing.com>
2024-12-03 19:02:59 +0530

    ksldfjas

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -69,7 +69,7 @@ development, their enthusiasm drives them towards continuous growth and innovati
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/profile.png"
+              src="/images/projects/profile.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
